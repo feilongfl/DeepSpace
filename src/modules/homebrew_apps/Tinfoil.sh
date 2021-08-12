81a1f348ea1f9e8a3ef316178aaf7ae37a92c947
YuLong Yao <feilongphone@gmail.com>
2021-08-13 06:36:43 +0800

    change tinfoil to self install

--- src/modules/homebrew_apps/Tinfoil.sh
+++ src/modules/homebrew_apps/Tinfoil.sh
@@ -1,11 +1,11 @@
 source src/utils/constants.sh
 source src/utils/logger.sh
 
-PKG_URL=http://tinfoil.media/repo/Tinfoil%20Applet%20Mode%20%5B12.10%5D%5Bv11%5D.zip
+PKG_URL=https://tinfoil.media/repo/Tinfoil%20Self%20Installer%20%5B050000BADDAD0000%5D%5B12.10%5D%5Bv11%5D.zip
 PKG_DIR=$ROOT_BUILD_PATH/tinfoil
 PKG_OUT_DIR=$ROOT_BUILD_PATH/tinfoil/out
 PKG_FILE_PATH=$PKG_DIR/Tinfoil.zip
-PKG_FILE_HASH=877499a312808b4fd9ef696a6b27b0c3
+PKG_FILE_HASH=fda17a58da7ae08facdb22bfb3d8862d
 
 fs_download $PKG_URL $PKG_FILE_PATH $PKG_FILE_HASH
 
