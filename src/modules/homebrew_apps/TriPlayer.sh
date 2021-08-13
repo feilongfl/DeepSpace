9fe2f31b2a836f9f0726577a46b9e7f0c3a21140
YuLong Yao <feilongphone@gmail.com>
2021-08-14 05:31:18 +0800

    fix path error

--- src/modules/homebrew_apps/TriPlayer.sh
+++ src/modules/homebrew_apps/TriPlayer.sh
@@ -2,8 +2,8 @@ source src/utils/constants.sh
 source src/utils/logger.sh
 
 PKG_URL=https://github.com/tallbl0nde/TriPlayer/releases/download/v1.1.1/triplayer-1.1.1.zip
-PKG_DIR=$ROOT_BUILD_PATH/tinfoil
-PKG_OUT_DIR=$ROOT_BUILD_PATH/tinfoil/out
+PKG_DIR=$ROOT_BUILD_PATH/triplayer
+PKG_OUT_DIR=$ROOT_BUILD_PATH/triplayer/out
 PKG_FILE_PATH=$PKG_DIR/triplayer.zip
 PKG_FILE_HASH=271ba0468d06a2a7c1fea5369efc2008
 
